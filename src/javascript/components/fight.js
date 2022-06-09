f207e2fd6bc9e5829965705180f0dbc24009a602
Hennadiy Khodyriev <khodirevgennadiy@gmail.com>
2022-06-09 18:46:24 +0300

    Created a math part in fight.js for hit power calculations

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -1,5 +1,14 @@
 import { controls } from '../../constants/controls';
 
+/*
+PlayerOneAttack: 'KeyA',
+PlayerOneBlock: 'KeyD',
+PlayerTwoAttack: 'KeyJ',
+PlayerTwoBlock: 'KeyL',
+PlayerOneCriticalHitCombination: ['KeyQ', 'KeyW', 'KeyE'],
+PlayerTwoCriticalHitCombination: ['KeyU', 'KeyI', 'KeyO']
+*/
+
 export async function fight(firstFighter, secondFighter) {
   return new Promise((resolve) => {
     // resolve the promise with the winner when fight is over
@@ -8,12 +17,25 @@ export async function fight(firstFighter, secondFighter) {
 
 export function getDamage(attacker, defender) {
   // return damage
+  let damage = getHitPower(attacker) - getBlockPower(defender);
+  if (damage <= 0) {damage = 0;}
+  return damage;
+
 }
 
 export function getHitPower(fighter) {
   // return hit power
+  let criticalHitChance = Math.random() + 1;
+  return hitPower = fighter.attack * criticalHitChance;
 }
 
 export function getBlockPower(fighter) {
   // return block power
+  let dodgeChance = Math.random() + 1;
+  return blockPower = fighter.defense * dodgeChance;
+}
+
+export function getCriticalHit(fighter) {
+  // return critical hit power
+  return fighter.attack * 2;
 }
