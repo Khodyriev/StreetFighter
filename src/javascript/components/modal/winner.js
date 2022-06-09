5b95153ea7082d56938bc2088b167aa7d8a858d8
Hennadiy Khodyriev <khodirevgennadiy@gmail.com>
2022-06-10 00:43:23 +0300

    Fixed styling

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,12 +1,18 @@
 import { createElement } from '../../helpers/domHelper';
 import { showModal } from './modal';
+import { createFighterImage } from '../fighterPreview';
+import App from '../../app';
 
 
 export function showWinnerModal(fighter) {
   // call showModal function 
   const title = `${fighter.name} is the ultimate winner!!!`;
-  const bodyElement = createElement('h1', 'modal-title');
-  bodyElement.innerText = 'Congrats!';
-  const onClose = () => window.location.reload();
-  showModal({ title, bodyElement, onClose });
+  const bodyElement = createFighterImage(fighter);;
+  
+  function startNewGame() {
+    const root = document.getElementById('root');
+    root.innerHTML = '';
+    App.startApp();
+  }
+  showModal({ title, bodyElement, onClose: () => startNewGame() });
 }
