e9044016a670b35cd28757f890128e6044701941
Hennadiy Khodyriev <khodirevgennadiy@gmail.com>
2022-06-10 00:07:05 +0300

    fixed some problem with build

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,12 +1,12 @@
 import { createElement } from '../../helpers/domHelper';
 import { showModal } from './modal';
-import { createFighterImage } from '.../../fighterPreview';
+
 
 export function showWinnerModal(fighter) {
   // call showModal function 
   const title = `${fighter.name} is the ultimate winner!!!`;
   const bodyElement = createElement('h1', 'modal-title');
-  bodyElement.innerHTML = createFighterImage(fighter);
+  bodyElement.innerText = 'Congrats!';
   const onClose = () => window.location.reload();
   showModal({ title, bodyElement, onClose });
 }
